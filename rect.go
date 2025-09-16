7d8499d1391cacf55a313f69c38e58ec39f3386d
Ibrahim Gursu Tekdemir <igtekdemir@gmail.com>
2025-09-16 17:39:56 +0300

    Update rect.go: init func StrokeRect

--- rect.go
+++ rect.go
@@ -10,3 +10,12 @@ func FillRect(w int, h int) {
 		fmt.Print("\n")
 	}
 }
+
+func StrokeRect(w int, h int) {
+	for i := 0; i < h; i++ {
+		for j := 0; j < w; j++ {
+			fmt.Print("@")
+		}
+		fmt.Print("\n")
+	}
+}
